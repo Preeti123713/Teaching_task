74154ce9607e19c307727627eaa8507bb70b8c65
Preeti123713 <nriya5892@gmail.com>
2023-11-03 19:01:37 +0530

    weekend work

--- wp-content/themes/twentytwentyone/template-pages/loginform.php
+++ wp-content/themes/twentytwentyone/template-pages/loginform.php
@@ -10,70 +10,93 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    <style>
+      .body-content{
+     margin: auto 0;
+      }
+    </style>
   </head>
   <body>
-   <!--Navbar-->
-   <h2>hello</h2>
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu">
-        <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div class="body-content">
+    <div id="form-outer">
+        <div id="frm-contact">
+          <form action="">
+            <div>
+                <input type="text" name="userName" id="userName"
+                    class="demoInputBox" PlaceHolder="Name">
+            </div>
+            <div>
+                <input type="text" name="userEmail" id="userEmail"
+                    class="demoInputBox" PlaceHolder="Email">
+            </div>
+            <div>
+                <input type="text" name="subject" id="subject"
+                    class="demoInputBox" PlaceHolder="Subject">
+            </div>
+            <div>
+                <textarea name="content" id="content"
+                    class="demoInputBox" rows="3" PlaceHolder="Content"></textarea>
+            </div>
+            <div id="mail-status">
+                <button name="submit" class="btnAction">Send</button>
+            </div>
+            </form>
         </div>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-<!--/.Navbar-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-  <script>
-    $(function(){
-    // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
-    $('.dropdown').on('show.bs.dropdown', function(e){
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-    });
+        <div id="btn-contact"> Contact Me</div>
+    </div>
 
-    // ADD SLIDEUP ANIMATION TO DROPDOWN //
-    $('.dropdown').on('hide.bs.dropdown', function(e){
-        e.preventDefault();
-        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(400, function(){
-            //On Complete, we reset all active dropdown classes and attributes
-            //This fixes the visual bug associated with the open class being removed too fast
-            $('.dropdown').removeClass('show');
-            $('.dropdown-menu').removeClass('show');
-            $('.dropdown').find('.dropdown-toggle').attr('aria-expanded','false');
-        });
-    });
-});
-    </script>
+    <div class="txt-content">
+        <p>Mauris blandit orci id risus tristique, non mattis ante
+            finibus. Duis volutpat tempor magna non posuere. Mauris a
+            vestibulum ligula, id commodo metus. Proin hendrerit, enim
+            at ullamcorper mattis, libero nisi blandit nulla, eu porta
+            tortor orci vel ipsum. Curabitur ullamcorper imperdiet lorem
+            nec pretium. Morbi finibus, mauris vitae feugiat euismod,
+            purus magna blandit nunc, ac tincidunt mi lacus eget leo.</p>
+    </div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+
+		$("#btn-contact").on("click", function() {
+
+			if ($("#frm-contact").is(":hidden")) {
+				$("#frm-contact").slideDown("slow");
+			} else {
+				$("#frm-contact").slideUp("slow");
+			}
+		});
+
+	});
+
+	function validateContact() {
+		var valid = true;
+		$(".demoInputBox").css('background-color', '');
+
+		if (!$("#userName").val()) {
+			$("#userName").css('background-color', '#f7dddd');
+			valid = false;
+		}
+		if (!$("#userEmail").val()) {
+			$("#userEmail").css('background-color', '#f7dddd');
+			valid = false;
+		}
+		if (!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+			$("#userEmail").css('background-color', '#f7dddd');
+			valid = false;
+		}
+		if (!$("#subject").val()) {
+			$("#subject").css('background-color', '#f7dddd');
+			valid = false;
+		}
+		if (!$("#content").val()) {
+			$("#content").css('background-color', '#f7dddd');
+			valid = false;
+		}
+
+		return valid;
+	}
+</script>
 </body>
 </html>
